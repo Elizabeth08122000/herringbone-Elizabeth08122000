bf53a0eefdb354b2b62c1f0fecf082540ce37d14
Liza <bliza0812@gmail.com>
2020-05-25 00:58:41 +0400

    Answer

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        Scanner in = new Scanner(System.in);
+        int number = in.nextInt();
+        int width = 0;
+        while(width < number){
+            for (int i = 0; i < number-width; i++) {
+                System.out.print(" ");
+            }
+            for (int i = 0; i < width*2 + 1; i++) {
+                System.out.print("*");
+            }
+            System.out.println();
+            width++;
+        }
     }
 }
